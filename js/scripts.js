3ba19a776e3a427f2cccbdb64eceaade919ec1eb
Michaela Davis <michaela.delight@gmail.com>
2018-03-26 19:17:18 -0700

    buttons on top switch images

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var imgContainer = document.getElementById('image');
   // add event listeners for each image on top
   var navButtons = document.querySelectorAll(".nav-button");
   [].forEach.call(navButtons, function(button) {
 
-
-
+    
     // add an event listener on click
     button.addEventListener("click", function(event){
       console.log(event.srcElement.dataset.imgUrl);
+      imgContainer.innerHTML = '';
+      img = document.createElement('img');
+      img.src = event.srcElement.dataset.imgUrl;
+      imgContainer.appendChild(img);
+
     });
-    // load an image into the main area
 
 
 
