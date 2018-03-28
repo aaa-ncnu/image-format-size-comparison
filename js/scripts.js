f6ca63e675ab6c2ad286313724d73bb2ebc9e4e1
Michaela Davis <michaela.delight@gmail.com>
2018-03-28 11:10:26 -0700

    SI-953: First pass at JS refactor
    Co-authored-by: Chad Brenner <digitalstack@gmail.com>
    Co-authored-by: Cody Craven <cody@cravencode.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,105 @@
-var appState = {};
 document.addEventListener("DOMContentLoaded", function() {
+  const imgContainer = document.querySelector('.image-viewer__viewer');
 
+  const navButtons = Array.from(document.querySelectorAll('.nav-button'));
 
-  var imgContainer = document.getElementById('image');
-  // add event listeners for each image on top
-  var navButtons = document.querySelectorAll(".nav-button");
-  [].forEach.call(navButtons, function(button) {
+  let currentImage = 0;
 
+  const filterState = {
+    density: 'standard',
+    showWebP: false,
+    imgSize: 768,
+    quality: [
+      40,
+      50,
+      60,
+      70,
+      80,
+      90
+    ]
+  };
 
-    // add an event listener on click
-    button.addEventListener("click", function(event){
-      // console.log(event.srcElement.dataset.imgUrl);
-      imgContainer.innerHTML = '';
-      img = document.createElement('img');
-      img.src = event.srcElement.dataset.imgUrl;
-      forWebPImage = event.srcElement.dataset.webpUrl;
+  function redrawImageQualityFilters() {
+    [40, 50, 60, 70, 80, 90].forEach(availableQuality => {
+      const el = document.querySelector(`input[name="quality-${availableQuality}"]`);
+      
+    });
+  }
+
+  function createImageSourceWithSize(imgSrc, width) {
+    // TODO: Remove `return imgSrc;` line when we have images.
+    return imgSrc;
+    const re = /(.*)\.([^.]+)$/;
+    const [, path, extension] = re.exec(imgSrc);
+    return `${path}-${width}w.${extension}`;
+  }
+
+  function createImageSourceWithQuality(imgSrc, quality) {
+    const re = /(.*)\.([^.]+)$/;
+    const [, path, extension] = re.exec(imgSrc);
+    return `${path}-${quality}q.${extension}`;
+  }
+
+  function clearElementChildren(element) {
+    Array.from(element.children).forEach(child => {
+      element.removeChild(child);
+    });
+  }
+
+  function showImage(selectedImage) {
+    currentImage = selectedImage;
+    clearElementChildren(imgContainer);
+
+    filterState.quality.forEach(quality => {
+      const img = document.createElement('img');
+      let src = createImageSourceWithSize(
+        navButtons[selectedImage].dataset.imgUrl,
+        filterState.imgSize
+      );
+      img.src = createImageSourceWithQuality(
+        src,
+        quality
+      );
       imgContainer.appendChild(img);
-      appState.imgSource = forWebPImage;
+    });
+  }
+
+  navButtons.forEach((el, index) => {
+    el.addEventListener('click', () => {
+      showImage(index)
     });
   });
 
-  // add event listeners for each option on the left
-  var webPcheckbox = document.getElementById('webP');
-  var webPimageContainer = document.getElementById('webPimage');
-  webPcheckbox.onchange = function(){
-    if (webPcheckbox.checked) {
-      webPimageContainer.innerHTML = '';
-      img = document.createElement('img');
-      img.src = appState.imgSource;
-      webPimageContainer.appendChild(img);
+  showImage(0);
 
-    } else {
-      webPimageContainer.innerHTML = '';
-    }
-  };
+
+  // // add event listeners for each image on top
+
+
+  //   // add an event listener on click
+  //   button.addEventListener("click", function(event){
+  //     // console.log(event.srcElement.dataset.imgUrl);
+  //     imgContainer.innerHTML = '';
+  //     img = document.createElement('img');
+  //     img.src = event.srcElement.dataset.imgUrl;
+  //     forWebPImage = event.srcElement.dataset.webpUrl;
+  //     imgContainer.appendChild(img);
+  //     appState.imgSource = forWebPImage;
+  //   });
+  // });
+
+  // // add event listeners for each option on the left
+  // var webPcheckbox = document.getElementById('webP');
+  // var webPimageContainer = document.getElementById('webPimage');
+  // webPcheckbox.onchange = function(){
+  //   if (webPcheckbox.checked) {
+  //     webPimageContainer.innerHTML = '';
+  //     img = document.createElement('img');
+  //     img.src = appState.imgSource;
+  //     webPimageContainer.appendChild(img);
+
+  //   } else {
+  //     webPimageContainer.innerHTML = '';
+  //   }
+  // };
 });
