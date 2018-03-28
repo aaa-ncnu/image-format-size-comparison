fce002295170a9a146a20f775120da5c1fca47ea
Michaela Davis <michaela.delight@gmail.com>
2018-03-27 20:23:46 -0700

    WebP works technically, add images :sunrise:

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
+var appState = {};
 document.addEventListener("DOMContentLoaded", function() {
+
+
   var imgContainer = document.getElementById('image');
   // add event listeners for each image on top
   var navButtons = document.querySelectorAll(".nav-button");
@@ -7,18 +10,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // add an event listener on click
     button.addEventListener("click", function(event){
-      console.log(event.srcElement.dataset.imgUrl);
+      // console.log(event.srcElement.dataset.imgUrl);
       imgContainer.innerHTML = '';
       img = document.createElement('img');
       img.src = event.srcElement.dataset.imgUrl;
+      forWebPImage = event.srcElement.dataset.webpUrl;
       imgContainer.appendChild(img);
-
+      appState.imgSource = forWebPImage;
     });
-
-
-
   });
 
   // add event listeners for each option on the left
+  var webPcheckbox = document.getElementById('webP');
+  var webPimageContainer = document.getElementById('webPimage');
+  webPcheckbox.onchange = function(){
+    if (webPcheckbox.checked) {
+      webPimageContainer.innerHTML = '';
+      img = document.createElement('img');
+      img.src = appState.imgSource;
+      webPimageContainer.appendChild(img);
 
+    } else {
+      webPimageContainer.innerHTML = '';
+    }
+  };
 });
